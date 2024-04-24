1bef148dc522f17c481def66c5af382fd2d15b59
Khanh Hua <giakhanh2487@gmail.com>
2024-04-24 16:53:32 +0200

    Find book

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
+import Prelude hiding (takeWhile)
+import System.IO (Handle, IOMode (ReadMode), hClose, hIsEOF, openFile)
 
-import System.IO (IOMode(ReadMode), Handle, hIsEOF, openFile, hClose)
-
-import qualified Data.Text as T (Text, breakOn, unwords, unpack, length, drop)
+import Control.Monad (foldM, replicateM, replicateM_, when)
+import Control.Monad.Reader (MonadIO (liftIO), MonadReader (ask), Reader, ReaderT (runReaderT), runReader)
+import Data.List (break)
+import Data.Maybe (fromJust, isJust)
+import qualified Data.Text as T (Text, breakOn, drop, length, unpack, unwords)
 import qualified Data.Text.IO.Utf8 as T (hGetLine)
-import Control.Monad.Reader (Reader, runReader, MonadReader (ask), ReaderT (runReaderT), MonadIO (liftIO))
-import Control.Monad (foldM, replicateM, replicateM_)
 
 data Verse = Verse
   { addr :: String
@@ -24,12 +26,22 @@ type VerseReaderT = ReaderT Handle IO (Maybe Verse)
 
 main :: IO ()
 main = do
+  bookname <- putStrLn "Bookname: " >> getLine
+  mbBook <- findBook bookname 
+  when (isJust mbBook) $ do
+    let book = fromJust mbBook
+    print $ "Found: " <> show (length $ verses book)
+    print $ "Name: " <> name book
+    print $ "Verse: " <> text (head $ verses book)
+
+findBook :: String -> IO (Maybe Book)
+findBook bookname = do
   hData <- openFile "data/kjv.txt" ReadMode
   replicateM_ 2 $ T.hGetLine hData
-  verses <- runReaderT (findAll (\v -> addr v == "Genesis 1:1")) hData
-  print $ "Found: " <> show (length verses)
-  print $ "Verse: " <> text (head verses)
+  mbBook <- runReaderT (selectBook bookname) hData 
   hClose hData
+
+  return mbBook
 
 verseReader :: VerseReaderT
 verseReader = do
@@ -42,19 +54,48 @@ verseReader = do
 
 findAll :: (Verse -> Bool) -> ReaderT Handle IO [Verse]
 findAll predicate = reverse <$> collect []
-  where
-    collect :: [Verse] -> ReaderT Handle IO [Verse]
-    collect acc = do
-      mbVerse <- verseReader
-      case mbVerse of
-        Nothing -> return acc
-        Just x ->
-          if predicate x
-            then collect (x:acc)
-            else collect acc
+ where
+  collect :: [Verse] -> ReaderT Handle IO [Verse]
+  collect acc = do
+    mbVerse <- verseReader
+    case mbVerse of
+      Nothing -> return acc
+      Just x ->
+        if predicate x
+          then collect (x : acc)
+          else collect acc
+
+takeWhile :: (Verse -> Bool) -> ReaderT Handle IO [Verse]
+takeWhile predicate = reverse <$> collect False []
+ where
+  collect :: Bool -> [Verse] -> ReaderT Handle IO [Verse]
+  collect found acc = do
+    mbVerse <- verseReader
+    case mbVerse of
+      Nothing -> return acc
+      Just x ->
+        if predicate x
+          then collect True (x : acc)
+          else if found
+            then return acc
+            else collect False acc
+
+
+selectBook :: String -> ReaderT Handle IO (Maybe Book)
+selectBook name = do
+  verses <- takeWhile ((==) name . bookname)
+  if null verses
+    then return Nothing
+    else return $ Just (Book name verses)
+
+bookname :: Verse -> String
+bookname (Verse address _) =
+  let (book, _) = break (' ' ==) address
+  in book
 
 parseVerse :: T.Text -> Verse
 parseVerse line =
   let
     (addr, text) = T.breakOn "\t" line
-  in Verse (T.unpack addr) (T.drop 1 text)
+   in
+    Verse (T.unpack addr) (T.drop 1 text)
